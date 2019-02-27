ae4421966fd5e401062f9d3fda084cdbe7cf2204
CyC2018 <zhengyc101@163.com>
2019-02-27 15:20:43 +0800

    Update content.js

--- public/js/content.js
+++ public/js/content.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
     var addSpaceBtn = $('#add-space-btn');
     var convertPicBtn = $('#convert-pic-btn');
     var checkResume = $('#check-resume');
+    
+    var keyword = '# 语言,Java,JDK,JVM,C++,STL,Golang,Python,PHP,MATLAB,C#,# Web 开发,Spring,SpringMVC,MyBatis,Hibernate,Struts2,Flask,Django,Tornado,# 数据库,MySQL,SQL,SQLServer,MongoDB,Redis,Memcached,Oracle,SQLite,# 前端开发,JavaScript,HTML,CSS,vue.js,jQuery,BootStrap,# 移动开发,Android,# 中间件,RabbitMQ,ZooKeeper,Kafka,Thrift,Storm,Streaming ,Samza,MyCat,Dubbo,RocketMQ,# 工具,Git,SVN,Maven,# 云计算,# 大数据,Hbase,Storm,Spark,Hadoop,Hive,# 运维,Linux,Unix,Nginx,Docker,KVM,OpenStack';
 
     /*
      * 添加空格
@@ -112,5 +114,5 @@ $(document).ready(function() {
         return '<div align="center">  <img src="' + picString + '" width=""/> </div><br>';
     }
 
-    var keyword = '# 语言,Java,JDK,JVM,C++,STL,Golang,Python,PHP,MATLAB,C#,# Web 开发,Spring,SpringMVC,MyBatis,Hibernate,Struts2,Flask,Django,Tornado,# 数据库,MySQL,SQL,SQLServer,MongoDB,Redis,Memcached,Oracle,SQLite,# 前端开发,JavaScript,HTML,CSS,vue.js,jQuery,BootStrap,# 移动开发,Android,# 中间件,RabbitMQ,ZooKeeper,Kafka,Thrift,Storm,Streaming ,Samza,MyCat,Dubbo,RocketMQ,# 工具,Git,SVN,Maven,# 云计算,# 大数据,Hbase,Storm,Spark,Hadoop,Hive,# 运维,Linux,Unix,Nginx,Docker,KVM,OpenStack';
+    
 });
