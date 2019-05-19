6b741e14a184c154a1d0190eb91ba6eb94881ae6
3G <googege@icloud.com>
2019-05-19 22:59:30 +0800

    Update content.js

--- public/js/content.js
+++ public/js/content.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     var convertPicBtn = $('#convert-pic-btn');
     var checkResume = $('#check-resume');
     
-    var keyword = '# 语言,Java,JDK,JVM,C++,STL,Golang,Python,PHP,MATLAB,C#,# Web 开发,Spring,SpringMVC,SpringBoot,SpringCloud,MyBatis,Hibernate,Struts2,Flask,Django,Tornado,# 数据库,MySQL,SQL,SQLServer,MongoDB,Redis,Memcached,Oracle,SQLite,# 前端开发,JavaScript,HTML,CSS,Vue.js,jQuery,BootStrap,# 后端开发,Node.js,# 移动开发,Android,# 中间件,RabbitMQ,ZooKeeper,Kafka,Thrift,Storm,Streaming ,Samza,MyCat,Dubbo,RocketMQ,# 工具,Git,SVN,Maven,# 云计算,# 大数据,HBase,Storm,Spark,Hadoop,Hive,# 运维,Linux,Unix,Nginx,Docker,KVM,OpenStack';
+    var keyword = '# 语言,Java,JDK,JVM,C++,STL,Golang,Python,PHP,MATLAB,C#,# Web 开发,Spring,SpringMVC,SpringBoot,SpringCloud,MyBatis,Hibernate,Struts2,Flask,Django,Tornado,# 数据库,MySQL,SQL,SQLServer,MongoDB,Redis,Memcached,Oracle,SQLite,# 前端开发,JavaScript,HTML,CSS,Vue.js,jQuery,BootStrap,HTTP,# 后端开发,Node.js,# 移动开发,Android,# 中间件,RabbitMQ,ZooKeeper,Kafka,Thrift,Storm,Streaming ,Samza,MyCat,Dubbo,RocketMQ,# 工具,Git,SVN,Maven,# 云计算,# 大数据,HBase,Storm,Spark,Hadoop,Hive,# 运维,Linux,Unix,Nginx,Docker,KVM,OpenStack';
 
     /*
      * 添加空格
