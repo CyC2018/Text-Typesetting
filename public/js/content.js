823485134cc0646c4b22f0572f1705704f0a116d
Fatbrother <kevin92422@gmail.com>
2023-11-06 19:01:26 +0800

    add more key words

--- public/js/content.js
+++ public/js/content.js
@@ -5,7 +5,28 @@ $(document).ready(function() {
     var convertPicBtn = $('#convert-pic-btn');
     var checkResume = $('#check-resume');
     
-    var keyword = '# 语言,Java,JDK,JVM,C++,STL,Golang,Python,PHP,MATLAB,C#,# Web 开发,Spring,SpringMVC,SpringBoot,SpringCloud,MyBatis,Hibernate,Struts2,Flask,Django,Tornado,# 数据库,MySQL,SQL,SQLServer,MongoDB,Redis,Memcached,Oracle,SQLite,# 前端开发,JavaScript,HTML,CSS,Vue.js,jQuery,BootStrap,# 后端开发,Node.js,# 移动开发,Android,# 中间件,RabbitMQ,ZooKeeper,Kafka,Thrift,Storm,Streaming ,Samza,MyCat,Dubbo,RocketMQ,# 工具,Git,SVN,Maven,# 云计算,# 大数据,HBase,Storm,Spark,Hadoop,Hive,# 运维,Linux,Unix,Nginx,Docker,KVM,OpenStack';
+    var keyword = '# 语言,\
+                        Java,JDK,JVM,C++,STL,Golang,Python,PHP,MATLAB,C#,Dart,Kotlin,Swift,Object-C,\
+                   # Web 开发,\
+                        Spring,SpringMVC,SpringBoot,SpringCloud,MyBatis,Hibernate,Struts2,Flask,Django,Tornado,\
+                   # 数据库,\
+                        MySQL,SQL,SQLServer,MongoDB,Redis,Memcached,Oracle,SQLite,PostgreSQL,Access,\
+                   # 前端开发,\
+                        JavaScript,HTML,CSS,Vue.js,jQuery,BootStrap,Flutter,React.js,Angular.js,\
+                   # 后端开发,\
+                        Node.js,\
+                   # 移动开发, \
+                        Android,IOS,\
+                   # 中间件,\
+                        RabbitMQ,ZooKeeper,Kafka,Thrift,Storm,Streaming,Samza,MyCat,Dubbo,RocketMQ,\
+                   # 工具,\
+                        Git,SVN,Maven,Gradle,Makefile,Shell,PowerShell,Visual Studio,IntelliJ IDEA,Eclipse,\
+                   # 云计算,\
+                        Google Cloud,AWS,Azure,OpenStack,\
+                   # 大数据,\
+                        HBase,Storm,Spark,Hadoop,Hive,\
+                   # 运维,\
+                        Linux,Unix,Nginx,Docker,KVM,OpenStack ';
 
     /*
      * 添加空格
